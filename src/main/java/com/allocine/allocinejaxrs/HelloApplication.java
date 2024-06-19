9f77b08e97e5705e2152a2293744b073a1c6241c
Nassim CHERFAOUI <99206668+NCherfaoui@users.noreply.github.com>
2024-06-20 00:13:16 +0200

    Create Maven project JAX-RS

---
+++ src/main/java/com/allocine/allocinejaxrs/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.allocine.allocinejaxrs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
